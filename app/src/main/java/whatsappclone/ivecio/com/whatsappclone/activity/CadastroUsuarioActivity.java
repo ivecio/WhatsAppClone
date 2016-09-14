31e4b085037234487ca30810e436ed3d08d785a3
Ivecio <Ivecio>
2016-09-13 21:32:51 -0300

    nova versão com Firebase

---
+++ app/src/main/java/whatsappclone/ivecio/com/whatsappclone/activity/CadastroUsuarioActivity.java
@@ -0,0 +1,14 @@
+package whatsappclone.ivecio.com.whatsappclone.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import whatsappclone.ivecio.com.whatsappclone.R;
+
+public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+    }
+}
