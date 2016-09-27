d837139f5355d70cff03c417b34463e721067e7f
Ivecio <Ivecio>
2016-09-26 22:13:34 -0300

    Adicionado menu no Main

--- app/src/main/java/whatsappclone/ivecio/com/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/whatsappclone/ivecio/com/whatsappclone/activity/MainActivity.java
@@ -2,13 +2,32 @@ package whatsappclone.ivecio.com.whatsappclone.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 import whatsappclone.ivecio.com.whatsappclone.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsAppClone");
+        setSupportActionBar( toolbar );
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
